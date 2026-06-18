21da71d1545f2397a65f7e5158bf46330bd9e878
André Hofer <a.hofer2k8@gmail.com>
2026-06-18 17:28:50 +0200

    feat: add cantons data module

---
+++ src/data/cantons.ts
@@ -0,0 +1,28 @@
+export interface Canton { code: string; name: string; w: string }
+
+const WBASE = 'https://commons.wikimedia.org/wiki/Special:FilePath/Wappen_';
+
+export const CANTONS: Canton[] = [
+  { code: 'ZH', name: 'Zürich', w: 'Zürich' }, { code: 'BE', name: 'Bern', w: 'Bern' },
+  { code: 'LU', name: 'Luzern', w: 'Luzern' }, { code: 'UR', name: 'Uri', w: 'Uri' },
+  { code: 'SZ', name: 'Schwyz', w: 'Schwyz' }, { code: 'OW', name: 'Obwalden', w: 'Obwalden' },
+  { code: 'NW', name: 'Nidwalden', w: 'Nidwalden' }, { code: 'GL', name: 'Glarus', w: 'Glarus' },
+  { code: 'ZG', name: 'Zug', w: 'Zug' }, { code: 'FR', name: 'Fribourg', w: 'Freiburg' },
+  { code: 'SO', name: 'Solothurn', w: 'Solothurn' }, { code: 'BS', name: 'Basel-Stadt', w: 'Basel-Stadt' },
+  { code: 'BL', name: 'Basel-Landschaft', w: 'Basel-Landschaft' }, { code: 'SH', name: 'Schaffhausen', w: 'Schaffhausen' },
+  { code: 'AR', name: 'Appenzell Ausserrhoden', w: 'Appenzell_Ausserrhoden' },
+  { code: 'AI', name: 'Appenzell Innerrhoden', w: 'Appenzell_Innerrhoden' },
+  { code: 'SG', name: 'St. Gallen', w: 'St._Gallen' }, { code: 'GR', name: 'Graubünden', w: 'Graubünden' },
+  { code: 'AG', name: 'Aargau', w: 'Aargau' }, { code: 'TG', name: 'Thurgau', w: 'Thurgau' },
+  { code: 'TI', name: 'Ticino', w: 'Tessin' }, { code: 'VD', name: 'Vaud', w: 'Waadt' },
+  { code: 'VS', name: 'Valais', w: 'Wallis' }, { code: 'NE', name: 'Neuchâtel', w: 'Neuenburg' },
+  { code: 'GE', name: 'Genève', w: 'Genf' }, { code: 'JU', name: 'Jura', w: 'Jura' },
+];
+
+export const cantonByCode = (code: string): Canton | undefined =>
+  CANTONS.find((c) => c.code === code);
+
+export const wappenUrl = (code: string): string => {
+  const c = cantonByCode(code);
+  return c ? WBASE + encodeURIComponent(c.w) + '_matt.svg' : '';
+};
